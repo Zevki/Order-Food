eacfde166e30f6ab46b6e861c0abf3bb2438fa44
Zenki <tqt2401@gmail.com>
2023-05-02 11:10:23 +0700

    8th update

--- app/src/main/java/com/example/appfood/ui/favourite/FavoriteFragment.java
+++ app/src/main/java/com/example/appfood/ui/favourite/FavoriteFragment.java
@@ -6,16 +6,64 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.appfood.R;
+import com.example.appfood.fragments.FragmentAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class FavoriteFragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter fragmentAdapter;
+    private FragmentActivity myContext;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(fragment_favourite, container, false);
 
+        tabLayout = root.findViewById(R.id.tab_layout);
+        viewPager2 = root.findViewById(R.id.view_pager2);
+
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+
+        viewPager2.setAdapter(fragmentAdapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Featured"));
+        tabLayout.addTab(tabLayout.newTab().setText("Popular"));
+        tabLayout.addTab(tabLayout.newTab().setText("News"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
         return root;
     }
 }
