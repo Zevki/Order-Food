c6621e78f9c82159ce7f35b6ed7a5b3d4e728e7c
phanhoangphuc0941 <mymyshroud113@gmail.com>
2023-05-03 22:09:15 +0700

    update

--- app/src/main/java/com/example/appfood/activities/DetailDaily.java
+++ app/src/main/java/com/example/appfood/activities/DetailDaily.java
@@ -37,17 +37,42 @@ public class DetailDaily extends AppCompatActivity {
         recyclerView.setAdapter(detailedDailyAdapter);
 
         if(type != null && type.equalsIgnoreCase("breakfast")){
-            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf1,"Breakfast","Description","4,6","45.000","10am to 24pm"));
-            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf2,"Breakfast","Description","4,6","45.000","10am to 24pm"));
-            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf3,"Breakfast","Description","4,6","45.000","10am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf_banhgio,"Bánh Giò","Description","4,0","25.000","10am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf_banhmi,"Bánh Mì","Description","4,6","25.000","10am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf_bunbo,"Bún Bò","Description","4.8","40.000","10am to 24pm"));
             detailedDailyAdapter.notifyDataSetChanged();
         }
         if(type != null && type.equalsIgnoreCase("sweet")){
 
             imageView.setImageResource(R.drawable.sweet);
-            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf1,"Sweet","Description","4,5","45.000","0am to 24pm"));
-            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf2,"Sweet","Description","4,6","45.000","0am to 24pm"));
-            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf3,"Sweet","Description","4,8","45.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.icecream_3vi,"Kem 3 vị","Description","4,5","25.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.sweet_chetroinuoc,"Chè Trôi Nước","Description","4,6","10.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.sweet_xoingot,"Xôi Ngọt","Description","4,8","15.000","0am to 24pm"));
+            detailedDailyAdapter.notifyDataSetChanged();
+        }
+        if(type != null && type.equalsIgnoreCase("lunch")){
+
+            imageView.setImageResource(R.drawable.lunch);
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.lunch_bunthitnuong,"Bún thịt nướng","Description","4,0","25.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.lunch_comtam,"Cơm Tấm","Description","4,5","35.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.lunch_comtron,"Cơm Trộn","Description","4,0","30.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.bf_bunbo,"Bún Bò","Description","4,8","40.000","0am to 24pm"));
+            detailedDailyAdapter.notifyDataSetChanged();
+        }
+        if(type != null && type.equalsIgnoreCase("dinner")){
+
+            imageView.setImageResource(R.drawable.dinner);
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.dinner_canhchuacakho,"Cơm Canh Chua Cá Kho","Description","4,9","40.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.dinner_thitkhotau,"Cơm Thịt Kho Tàu","Description","4,3","30.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.dinner_tomrangthit,"Cơm Tôm Rang Thịt","Description","4,","35.000","0am to 24pm"));
+            detailedDailyAdapter.notifyDataSetChanged();
+        }
+        if(type != null && type.equalsIgnoreCase("coffee")){
+
+            imageView.setImageResource(R.drawable.coffe);
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.coffee_bacxiu,"Coffee Bạc Xỉu","Description","4.6","25.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.coffesua,"Coffee Sữa","Description","4,5","20.000","0am to 24pm"));
+            detailedDailyModelList.add(new DetailedDailyModel(R.drawable.coffee_den,"Coffee Đen","Description","4,7","15.000","0am to 24pm"));
             detailedDailyAdapter.notifyDataSetChanged();
         }
 
